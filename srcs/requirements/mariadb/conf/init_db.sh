4b6db455df8373d72360446c03fef9da9d518bd3
gansard <gregoire.ansard@gmail.com>
2023-10-31 11:41:14 -0400

    day - 00

---
+++ srcs/requirements/mariadb/conf/init_db.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Démarrage de MySQL
+service mysql start;
+
+# Création de la base de données
+mysql -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+
+# Création d'un utilisateur pour la base de données
+mysql -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Octroi de tous les droits à l'utilisateur pour la base de données
+mysql -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_PASSWORD}';"
+
+# Modification des droits de l'utilisateur root
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
+
+# Rafraîchissement des privilèges
+mysql -e "FLUSH PRIVILEGES;"
+
+# Arrêt de MySQL
+mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
+
+# Démarrage de MySQL avec mysqld_safe
+exec mysqld_safe
